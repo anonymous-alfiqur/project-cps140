d1056867951b9b89943cf8d96bbadad1826f48fe
anonymous-alfiqur <alfiqurrownok@gmail.com>
2024-11-14 12:33:40 -0500

    add date and time

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log('Hello!');
+// Data block
+
+const todaysDate = () => {
+    let d = new Date();
+    return "Today's date is" + d
+}
+
+const dateBlock = document.getElementById("dateBlock")
+dateBlock.innerText = todaysDate();
+const body = document.body
+const d = new Date();
+
+if (d.getHours() > 20 || d.getHours() < 8) {
+    body.styles["backgroundColor"] = "black";
+    body.style["color"] = "white";
+} else {
+    Body.style["backgroundColor"] = "white";
+    body.style["color"] = "black";
+}
