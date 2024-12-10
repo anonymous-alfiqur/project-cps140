410f14019dca5b644c599cee87a313ecc8afd7de
anonymous-alfiqur <alfiqurrownok@gmail.com>
2024-12-10 12:20:21 -0500

    wesite redesign

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-console.log('Hello!');
-// Data block
+// console.log('Hello!');
+// // Data block
 
-const todaysDate = () => {
-    let d = new Date();
-    return "Today's date is" + d
-}
+// const todaysDate = () => {
+//     let d = new Date();
+//     return "Today's date is" + d
+// }
 
-const dateBlock = document.getElementById("dateBlock")
-dateBlock.innerText = todaysDate();
-const body = document.body
-const d = new Date();
+// const dateBlock = document.getElementById("dateBlock")
+// dateBlock.innerText = todaysDate();
+// const body = document.body
+// const d = new Date();
 
-if (d.getHours() > 20 || d.getHours() < 8) {
-    body.styles["backgroundColor"] = "black";
-    body.style["color"] = "white";
-} else {
-    body.style["backgroundColor"] = "white";
-    body.style["color"] = "black";
-}
+// if (d.getHours() > 20 || d.getHours() < 8) {
+//     body.styles["backgroundColor"] = "black";
+//     body.style["color"] = "white";
+// } else {
+//     body.style["backgroundColor"] = "white";
+//     body.style["color"] = "black";
+// }
